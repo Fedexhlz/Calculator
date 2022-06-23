b8376fdd3791065018508edac99d2d398afdaeaf
Federico-Herrera <fedex.hlz@gmail.com>
2022-06-22 21:18:44 -0300

    Add display constructor

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const previousValue = document.getElementById('previous-value');
-const currentValue = document.getElementById('current-value');
+const displayPreviousValue = document.getElementById('previous-value');
+const displayCurrentValue = document.getElementById('current-value');
 const numberButton = document.querySelectorAll('.number');
 const operatorButton = document.querySelectorAll('operator');
 
-const calculator = new Calculator();
+const display = new Display(displayPreviousValue, displayCurrentValue);
--- src/js/Display.js
+++ src/js/Display.js
@@ -1,7 +1,13 @@
 class Display {
-  constructor(previousValue, currentValue) {
-    this.currentValue = currentValue;
-    this.previousValue = previousValue;
+  constructor(displayPreviousValue, displayCurrentValue) {
+    this.displayCurrentValue = displayCurrentValue;
+    this.displayPreviousValue = displayPreviousValue;
     this.calculator = new Calculator();
+    this.currentValue = '';
+    this.previousValue = '';
+  }
+
+  addNumber(number) {
+    this.currentValue = number;
   }
 }
