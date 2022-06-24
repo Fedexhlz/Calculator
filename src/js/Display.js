24be64de217363b93fa192599a81dcd84bc593c8
Federico-Herrera <fedex.hlz@gmail.com>
2022-06-23 22:28:47 -0300

    Add dot, backspace and delete button

--- src/js/Display.js
+++ src/js/Display.js
@@ -3,8 +3,21 @@ class Display {
     this.displayCurrentValue = displayCurrentValue;
     this.displayPreviousValue = displayPreviousValue;
     this.calculator = new Calculator();
+    this.operationType = undefined;
     this.currentValue = '';
     this.previousValue = '';
+  }
+
+  delete() {
+    this.currentValue = this.currentValue.toString().slice(0, -1);
+    this.printValue();
+  }
+
+  deleteAll() {
+    this.currentValue = '';
+    this.previousValue = '';
+    this.operationType = undefined;
+    this.printValue();
   }
 
   addNumber(number) {
